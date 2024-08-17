1b70341e42e0813475c494f3fa2af281ce6d94b2
StanciuMihai <misu_sergiu@yahoo.com>
2024-08-17 21:11:52 +0300

    Added "Add To do" feature

--- src/AddItem.vue
+++ src/AddItem.vue
@@ -1,7 +1,37 @@
 <script setup>
+import { ref } from 'vue';
+import { useToast } from 'vue-toastification';
 
+const text= ref('');
+
+
+const emit = defineEmits(['transactionSubmitted']);
+
+const toast=useToast();
+
+  const onSubmit = () => {
+
+    if(!text.value){
+      toast.error('Please enter something to do before submitting');
+      return;
+    }
+
+    
+   
+    
+    const transactionData={
+      text:text.value
+    }
+    emit('transactionSubmitted',transactionData);
+   
+    text.value='';
+
+  }
 </script>
 
 <template>
-    
+     <form id="form" @submit.prevent="onSubmit">
+        <input type="text" id="text" v-model="text" placeholder="Add something to do..." />
+        <button class="btn">Add</button>
+     </form>
 </template>
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
   import Header from './Header.vue';
   import ListElements from './ListElements.vue';
+  import AddItem from './AddItem.vue';
   import {ref,computed, onMounted} from 'vue';
+  import { useToast } from 'vue-toastification';
+
+  const toast=useToast();
 
   const fillerList = ref([{
         id:1,
@@ -19,10 +23,40 @@
         isChecked: false
     },
     ]);
+
+    const handleTransactionSubmitted = (transactionData) => {
+    
+    fillerList.value.push({
+      id: generateUniqueId(),
+      value: transactionData.text,
+      isChecked: false
+    });
+
+   // saveTransactionsToLocalStorage();
+
+    toast.success('To do added');
+  };
+
+  const handleTransactionDeleted = (id) => {
+    transactions.value=transactions.value.filter((transaction)=>
+    transaction.id !==id)
+
+    //saveTransactionsToLocalStorage();
+
+    toast.success('To do deleted');
+  };
+
+  const generateUniqueId =() => {
+    return Math.floor(Math.random()* 1000000)
+  }
+  // const saveTransactionsToLocalStorage=()=>{
+  //   localStorage.setItem('transactions',JSON.stringify(transactions.value));
+  // }
 </script>
 
 <template>
   <Header />
+  <AddItem @transaction-submitted="handleTransactionSubmitted"/>
   <ListElements :fillerList="fillerList"/>
 </template>
 
